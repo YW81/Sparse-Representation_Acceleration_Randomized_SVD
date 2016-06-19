f7a02b8053fe7ed41418da84b88e14407fec94fa
haru <tim.zhe@gmail.com>
2016-06-19 10:34:20 +0800

    sweeping codes for cifar and caltech modified

--- main/sweep_data/cifar10.m
+++ main/sweep_data/cifar10.m
@@ -1,8 +1,8 @@
 %BIOINFORMATIC
-cd ../../
-load('Data/cifar-10-batches-mat/data_batch_1.mat')
-[Y,A,trainClass,targetClass] = preproMat(double(data'),double(labels)+1,0.5);
+addpath('../')
+load('../../Data/cifar-10-batches-mat/data_batch_1.mat')
+[Y,A,trainClass,targetClass] = preproMat(double(data'),double(labels)+1,0.1);
 clearvars -except Y A trainClass targetClass
-step = floor(size(Y,2)/20);
+step = floor(min(size(Y,1),size(Y,2))/20);
 sweep_steps = step:step:15*step;
-compare_svd_sr(Y,A,trainClass,targetClass,sweep_steps,'result/bioinformatic_sweep.mat')
+compare_svd_sr(Y,A,trainClass,targetClass,sweep_steps,'../../result/bioinformatic_sweep.mat')
